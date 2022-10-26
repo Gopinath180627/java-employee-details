c755d8c6ebc6270aa0cff6e41893a64d4ba394b3
Gopinath180627 <gopinathk1806@gmail.com>
2022-10-26 13:26:41 +0530

    studentDetails

---
+++ Student.java
@@ -0,0 +1,13 @@
+package org.student;
+
+public class Student {
+public void StudentDetails() {
+System.out.println("Student Name: ");
+}
+	public static void main(String[] args) {
+		Student s = new Student();
+		s.StudentDetails();
+
+	}
+
+}
